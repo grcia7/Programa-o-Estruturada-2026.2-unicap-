fef6c4d7b8068a7b0a047147b8aeac68525b2313
Eduardo Garcia <edu.j.garcia.08@gmail.com>
2026-08-13 12:01:40 +0000

    else

--- aulasprofestru/ex03-for.c
+++ aulasprofestru/ex03-for.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-#define VALOR_INICIAL 2
-#define VALOR_MAXIMO 50
-#define VALOR_INCREMENTO 2
+#define VALOR_INICIAL 1
+#define VALOR_MAXIMO 51
+#define VALOR_INCREMENTO 1
 
 
 int main () {
     for (int i = VALOR_INICIAL; i < VALOR_MAXIMO ; i+= VALOR_INCREMENTO) {
-        printf("%d", i);
+        if ( i % 3 == 0 || i % 10 == 3) {
+            printf("PI");
+        } else {
+            printf("%d", i);
+        }
         if (i <VALOR_MAXIMO) {
                 printf(" , ");
         }
