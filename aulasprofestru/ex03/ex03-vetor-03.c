09907b612c207fce917b17b60db41f290abf769b
Eduardo Garcia <edu.j.garcia.08@gmail.com>
2026-09-10 14:31:18 +0000

    Organizaçao e ordenaçao de vertor

--- aulasprofestru/ex03/ex03-vetor-03.c
+++ aulasprofestru/ex03/ex03-vetor-03.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define TAM 20
+#define TAM 7
 
 void imprimirVetor(int v[], int n);
 void preencherOrdenadoDigitacao(int v[], int n);
@@ -34,5 +34,34 @@ void preencherOrdenadoDigitacao(int v[], int n) {
 }
 
 void inserirOrdenado(int v[], int n, int x) {
+    int i = n - 1;
+    while (i >= 0  && v[i] > x) {
+        v[i + 1] = v[i];
+        i -= 1;
+    }
+    v[i + 1] = x;
+}
 
+int buscaSequencialMelhorada(int v[], int n, int x) {
+    int i;
+    for (int i = 0; i < n; i += 1) {
+        if (v[i] >= x) {
+            break;
+        }
+    }
+    if ( i < n && v[i] == x){
+        return i;
+}
+    return -1;
+}
+int buscaSequencialSentinela(int v[], int n, int x){
+    int i = 0;
+    v[n] = x; // vetor precisa ter capacidade de pelo menos 1 a mais que o tamanho
+    while (v[i] !=x ){
+        i += 1;
+    }
+    if ( i< n) {
+        return i;
+    }
+    return -1;
 }
